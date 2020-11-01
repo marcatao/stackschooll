c65b9fca65aa1a2306f8a8c75c3a8151fb03baac
marcatao <thiagomarcato@gmail.com>
2020-11-01 18:40:22 -0300

    wellcome email

--- backend/laravel_app/app/Mail/WellcomeMail.php
+++ backend/laravel_app/app/Mail/WellcomeMail.php
@@ -28,6 +28,6 @@ class WellcomeMail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.wellcome');
+        return $this->view('emails.wellcome.wellcome');
     }
 }
