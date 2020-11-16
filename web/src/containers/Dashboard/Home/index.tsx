e05633ced238f5e372a19535abd8a250966c456d
Anderson <mr.anderson.brito@gmail.com>
2020-11-16 16:00:00 -0300

    Web: Create Home page in Dashboard

---
+++ web/src/containers/Dashboard/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Title } from "../../../components/Title";
+import { HomeWrapper } from "./styles";
+
+const Home = (): JSX.Element => {
+  return (
+    <HomeWrapper>
+      <Title>Bem Vindo</Title>
+    </HomeWrapper>
+  );
+};
+
+export { Home };
--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,14 +1,3 @@
-import React from "react";
-
-import { Title } from "../../components/Title";
-import { HomeWrapper } from "./styles";
-
-const Home = (): JSX.Element => {
-  return (
-    <HomeWrapper>
-      <Title>Bem Vindo</Title>
-    </HomeWrapper>
-  );
-};
+import { Home } from "../../containers/Dashboard/Home";
 
 export default Home;
